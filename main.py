5e6f992140a397b57e3fc7de290b82e1d1018c1c
iagorichard <iagorrs@gmail.com>
2024-12-30 13:47:11 -0300

    adapting the copy pdfs addind rename_files parameter

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from services.pdf_library import PDFLibrary
 
 def main():
     
+    # Makes the parser of the PDF library
     parser = argparse.ArgumentParser(description="PDF Library Tool")
     parser.add_argument("--option", required=True, help="Operation to perform (e.g., copy_all)")
     parser.add_argument("--folder_in", help="Path to the input folder")
     parser.add_argument("--folder_out", help="Path to the output folder")
+    parser.add_argument("--rename_files", help="Path to the output folder")
 
+    # Parses the arguments from the command line and stores them in the 'args' object
     args = parser.parse_args()
 
+    # Checks the validity of the provided arguments and performs the required operation
     if args.option == "copy_all":
         if not args.folder_in or not args.folder_out:
             raise ValueError("For 'copy_all', both '--folder_in' and '--folder_out' must be specified.")
         
-        # Executar o método copy_pdfs
         print(f"Executing 'copy_all' with input folder: {args.folder_in} and output folder: {args.folder_out}")
-        PDFLibrary.copy_pdfs(args.folder_in, args.folder_out)
-    
+        
+        if not args.rename_files:
+            PDFLibrary.copy_pdfs(args.folder_in, args.folder_out)    
+        else:
+            PDFLibrary.copy_pdfs(args.folder_in, args.folder_out, args.rename_files) 
     else:
         raise ValueError(f"Unknown option: {args.option}. Supported options: 'copy_all'.")
 
--- services/pdf_library.py
+++ services/pdf_library.py
@@ -4,7 +4,7 @@ import shutil
 class PDFLibrary():
 
     @staticmethod
-    def copy_pdfs(input_folder, output_folder):
+    def copy_pdfs(input_folder, output_folder, rename_files=None):
         """
         Copies all PDFs from a folder and its subfolders to an output folder.
         
@@ -16,11 +16,19 @@ class PDFLibrary():
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
         
+        f_count = 0
+
         for root, _, files in os.walk(input_folder):
             for file in files:
                 if file.lower().endswith('.pdf'):
+                    f_count += 1
+                    out_file = file
+                    
+                    if rename_files is not None:
+                        out_file = rename_files + str(f_count).zfill(4) + ".pdf"
+
                     source_path = os.path.join(root, file)
-                    destination_path = os.path.join(output_folder, file)
+                    destination_path = os.path.join(output_folder, out_file)
                     
                     shutil.copy2(source_path, destination_path)
                     print(f"Copying: {source_path} -> {destination_path}")
